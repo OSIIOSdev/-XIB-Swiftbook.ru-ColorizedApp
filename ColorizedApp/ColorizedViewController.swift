910c8aea936e8193cb0c4882d80c8986c5fbba42
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-07 20:34:42 -0800

    1. Project prepared

---
+++ ColorizedApp/ColorizedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by OSIIOSdev on 07.03.2025.
+//
+
+import UIKit
+
+final class ColorizedViewController: UIViewController {
+    
+    // MARK: - @IBOutlets
+    
+    
+    
+    // MARK: - Private properties
+    
+    
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - @IBActions
+    
+    
+    
+    // MARK: - Private methods
+
+
+}
+
